66034d774cedb1d8b4c47a4580410a125036bdd2
Arfin Hosain <developerarfin@gmail.com>
2022-09-26 20:23:43 +0600

    Initial commit

---
+++ app/src/main/java/com/example/dictionaryapp/feature_dictionary/di/WordInfoApplication.kt
@@ -0,0 +1,8 @@
+package com.example.dictionaryapp.feature_dictionary.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WordInfoApplication :Application() {
+}
